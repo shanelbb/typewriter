e7a4bf5120975eee1ce75301611a401672b7e47d
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-19 00:20:40 +0000

    fixed \n code

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,7 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
 
 for (let i = 0; i < sentence.length; i++) {
   setTimeout(() => {
     process.stdout.write(sentence[i]);
   }, i * 100);
 }
-setTimeout(() => {
-  process.stdout.write('\n');
-}, sentence.length * 100);
